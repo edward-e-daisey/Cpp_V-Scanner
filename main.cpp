1ebf025a553ed8a53e49870d3b5dcef10f020257
edward-e-daisey <121644982+edward-e-daisey@users.noreply.github.com>
2023-01-21 18:07:38 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@
 #include <fstream>
 #include <string>
 
-// Function to scan a file for viruses.
+// Purpose - Function to scan a file for viruses.
 bool scan_file(std::string fileName){
     std::ifstream file(fileName);
     std::string line;
     while (std::getline(file,line)){
-        // Check for known virus signature in the file.
+        // Purpose - Check for known virus signature in the file.
         if (line.find("VirusSignatureA")!=std::string::npos ||
             line.find("VirusSignatureB")!=std::string::npos ||
             line.find("VirusSignatureC")!=std::string::npos) {
@@ -25,11 +25,11 @@ bool scan_file(std::string fileName){
 
 int main(){
 std::cout << "Starting antivirus scan..." << std::endl;
-    // Scan all files in the current directory.
+    // Purpose - Scan all files in the current directory.
     std::string fileName;
     for (int i = 0; i < 1; i++){
         fileName = "file" + std::to_string(i) + ".txt";
-        // Quarantine the infected file.
+        // Purpose - Quarantine the infected file.
         std::cout << "Quarantining infected file: " << fileName << std::endl;
         // To Do - Add Quarantine Function
     }
